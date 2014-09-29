0b3079d91cb7c768446fc783efc08221acdb565f
Larry Wu <lw7360@gmail.com>
2014-09-29 10:59:05 -0700

    Basic camera

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-console.log('hi');
+(function() {
+        var asciiContainer = document.getElementById("video");
+        var capturing = false;
+
+        camera.init({
+                width: 160,
+                height: 120,
+                fps: 30,
+                mirror: true,
+
+                onFrame: function(canvas) {
+                        ascii.fromCanvas(canvas, {
+                                // contrast: 128,
+                                callback: function(asciiString) {
+                                        asciiContainer.innerHTML = asciiString;
+                                }
+                        });
+                },
+
+                onSuccess: function() {
+                        capturing = true;
+                        asciiContainer.style.display = "block";
+                },
+
+                onNotSupported: function() {
+                        document.getElementById("info").style.display = "none";
+                        asciiContainer.style.display = "none";
+                        document.getElementById("notSupported").style.display = "block";
+                }
+        });
+})();
